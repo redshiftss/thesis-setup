a4813744a06af81e92154be170c54f8dc21a94f2
Laura Pircalaboiu <laura.pirca@gmail.com>
2023-05-07 10:49:15 +0200

    add results to gitignore

--- src/aggregate_docker_ports.sh
+++ src/aggregate_docker_ports.sh
@@ -1,1 +1,2 @@
 docker ps --format "{{.Ports}}" | awk -F '->' '{print $1}'
+docker ps --format "{{.Image}} {{.Ports}}" | awk -F '->' '{print $1}'
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,65 @@
 extern crate rs_docker;
 
-use std::{sync::{Mutex, Arc}, thread::scope};
+use std::{sync::{Mutex, Arc}, thread::{scope, self}, time::{self, UNIX_EPOCH, Duration}, fs::{self, File}, io::Write};
 use std::process::Command;
-use rs_docker::{Docker, image::Image};
-use ureq::{Agent, AgentBuilder};
-use chrono::{DateTime,  Utc};
+use std::time::SystemTime;
+
 fn main() {
-    let mut docker = match Docker::connect("unix:///var/run/docker.sock") {
-    	Ok(docker) => docker,
-        Err(e) => { panic!("{}", e); }
-    };
-
-    // let images = match docker.get_images(true) {
-    //     Ok(images) => images,
-    //     Err(e) => { panic!("{}", e); }
-    // };
-
     run_pipeline(1);
-    run_pipeline(2);
-    run_pipeline(3);
-    run_pipeline(4);
-
-    // let cores = 10;
-    // let image_queue = Arc::new(Mutex::new(images));
-    // scope(|s| {
-    //     for _ in 0..cores {
-    //         let cqc = image_queue.clone();
-    //         s.spawn(move ||{
-    //             while let Some(image) = {
-    //                 let mut guard = cqc.lock().unwrap();
-    //                 let container = guard.pop();
-    //                 drop(guard);
-    //                 container
-    //             } {
-    //                 // analyze_image(image);
-    //             }
-    //         });
-    //     }
-    // });
 }
 
 fn run_pipeline(pagenum: u32){
-    aggregate_and_pull_images(pagenum);
+    cleanup();
+    let imgs = aggregate_and_pull_images(pagenum);
     aggregate_ports();
     run_nuclei(pagenum);
-    cleanup();
+    run_trivy(pagenum, imgs);
 }
 
-fn analyze_image(selected: Image) {
-    let output = Command::new("bash")
-        .arg("-c")
-        .arg(format!("trivy image -f json {}", selected.RepoTags[0]))
-        .output()
-        .expect("failed to execute trivy");
-    let h = String::from_utf8(output.stdout).unwrap();
-    println!("{}", h);
+#[derive(Debug)]
+struct MyImage {
+    name : String,
+    ips : Vec<String>
 }
 
-fn aggregate_and_pull_images(pagenum: u32) {
-    let body: serde_json::Value = ureq::get(&format!("https://hub.docker.com/api/content/v1/products/search?page={}&page_size=25&q=", pagenum))
+struct Vulnerability {
+    severity : String,
+    cvenum : String,
+}
+
+struct ImageReport {
+    image_name : String,
+    vuln_list_static : Vec<Vulnerability>,
+    vuln_list_dynamic : Vec<Vulnerability>
+}
+
+fn run_trivy(pagenum : u32, images : Vec<MyImage>) {
+    let mut res = String::new();
+    println!("{:?}", images);
+
+    for image in images {
+        let output = Command::new("bash")
+                .arg("-c")
+                .arg(format!("trivy image -f json {}", image.name))
+                .output()
+                .expect("failed to execute trivy");
+        let h = format!("{},\n\n", String::from_utf8(output.stdout).unwrap());
+        res.push_str(&h);
+    }
+
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+
+    let filename = format!("trivy/results_{:?}_{}.json", since_the_epoch, pagenum);
+
+    let mut file = File::create(filename).unwrap();
+    file.write_all(res.as_bytes()).unwrap();
+}
+
+fn aggregate_and_pull_images(pagenum: u32) -> Vec<MyImage> {
+    let body: serde_json::Value = ureq::get(&format!("https://hub.docker.com/api/content/v1/products/search?page={}&page_size=5&q=", pagenum))
     .set("Search-Version", "v3")
     .call()
     .unwrap()
@@ -84,17 +85,45 @@ fn aggregate_and_pull_images(pagenum: u32) {
         .output()
         .expect("failed to run image");
 
-
         // let h = String::from_utf8(output.stdout).unwrap();
         // println!("{}", h);
-        println!("finished pulling images...")
+        println!("finished pulling images...");
     }
 
+    thread::sleep(Duration::from_secs(1));
+
+    let output = Command::new("bash")
+    .arg("-c") 
+    .arg("docker ps --format \"{{.Image}} {{.Ports}}\" | awk -F \'->\' \'{print $1}\'")
+    .output()
+    .expect("failed to aggregate ports");
+
+    let mut imgs = Vec::new();
+        
+    let str = String::from_utf8(output.stdout).unwrap();
+    
+    let lines = str.split("\n");
+
+    for line in lines {
+        let tokens = line.split(" ").collect::<Vec<&str>>();
+        let name = tokens[0];
+        let mut ips = Vec::new();
+        let mut i = 1;
+
+        while i < tokens.len() {
+            ips.push(tokens[i].to_string());
+            i+=1;
+        }
+        if (name != ""){
+            imgs.push(MyImage { name: name.to_string(), ips: ips })
+        }
+    }
+    return imgs;
 }
 
 fn aggregate_ports(){
     Command::new("bash")
-    .arg("-c")
+    .arg("-c") 
     .arg("docker ps --format \"{{.Ports}}\" | awk -F \'->\' \'{print $1}\' > ports.txt")
     .output()
     .expect("failed to aggregate ports");
@@ -102,15 +131,18 @@ fn aggregate_ports(){
 }
 
 fn run_nuclei(pagenum: u32){
-    let filename = format!("results_{}_{}.json", Utc::now(), pagenum);
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+
+    let filename = format!("nuclei/results_{:?}_{}.json", since_the_epoch, pagenum);
     println!("filename: {}", filename);
     Command::new("bash")
     .arg("-c")
-    .arg(format!("~/go/bin/nuclei -l ports.txt  -ni -je {}", filename))
+    .arg(format!("~/go/bin/nuclei -l ports.txt -hm -ni -je {}", filename))
     .status()
     .expect("failed to run nuclei");
-
-    // let h = String::from_utf8(output.stdout).unwrap();
 }
 
 fn cleanup() {
